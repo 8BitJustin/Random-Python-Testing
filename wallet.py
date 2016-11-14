66a5afa0633c373d7c77c4f0ac5bee1ca5d1a42f
Justin Olson <jro34@live.com>
2016-11-13 21:08:48 -0700

    Added apostrophe to line 6 of wallet, and gave discription

--- wallet.py
+++ wallet.py
@@ -1,6 +1,7 @@
 wallet = 15
 
 def buy_game():
+    """Silly script to indicate to buy a game if the funds in wallet were avail."""
     if wallet >= 13.37:
         print('Im\'ma get that game!')
     else:
